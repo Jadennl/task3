91e2db656cb7bd0706f8a673eb90029738b9b89d
Jaden <Jnleonard@hotmail.com>
2019-05-30 11:25:23 -0400

    Trial code worked successfully! merge soon.

--- manipulation.hs
+++ manipulation.hs
@@ -7,3 +7,7 @@ method2 :: [String] -> [String]
 method2 [] = []
 method2 [word] = [word]
 method2 (word1:word2:words) = method1 word1 1 : method1 word2 2 : method2 words
+
+trial :: String -> [String]
+trial [] = []
+trial word = take 20 word : (trial $ drop 20 word)
