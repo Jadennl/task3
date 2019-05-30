a29a3f15be156a49ea7c1148d251ee3ba4057412
Jaden <Jnleonard@hotmail.com>
2019-05-30 11:06:16 -0400

    first commit

---
+++ manipulation.hs
@@ -0,0 +1,9 @@
+method1 :: String -> Int -> String
+method1 word num
+    | even num = word
+    | otherwise = reverse word
+
+method2 :: [String] -> [String]
+method2 [] = []
+method2 [word] = [word]
+method2 (word1:word2:words) = method1 word1 1 : method1 word2 2 : method2 words
